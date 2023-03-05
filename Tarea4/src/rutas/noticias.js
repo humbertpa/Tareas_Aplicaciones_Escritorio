892c6a98285cde18022617a21ad6c3c09400dff0
humbertpa <humbertpal@gmail.com>
2023-03-04 19:43:27 -0600

    ya tiene estilo

--- Tarea4/src/rutas/noticias.js
+++ Tarea4/src/rutas/noticias.js
@@ -9,13 +9,14 @@ const api_key = process.env.NEWSAPI_KEY;
 router.get('', async (req, res) => {
     //const url = 'https://newsapi.org/v2/everything?q='+req.body+ api_key;
 
-    const url = 'https://newsapi.org/v2/everything?q=paletas&apiKey=' + api_key;
+    const url = 'https://newsapi.org/v2/everything?q=baking&apiKey=' + api_key;
     console.log(url);
     const response = await fetch(url);
     const json = await response.json();
     //console.log(Object.keys(json));
     //console.log(json); //archivo completo
     //console.log(json.articles); //solo los artículos
+    console.log(Object.keys(json.articles[0]));
     let contenido = json.articles.map((element) => {
 
         let html = `
@@ -23,14 +24,15 @@ router.get('', async (req, res) => {
             <img src="${element.urlToImage}" alt="Avatar" style="width:100%">
                 <div class="container">
                     <h4><b>${element.title}</b></h4> 
-                    <p>${element.url}</p> 
+                    <p>${element.description}</p> 
+                    <p>${element.publishedAt}</p> 
+                    <a href="${element.url}">A través de ${element.source.name}</a>
                 </div>
             </div>
             `;
         return (html);
     }).join(``);
 
-    console.log(contenido);
     res.render('noticias', { layout: '../noticias', datos: contenido });
 
 });
