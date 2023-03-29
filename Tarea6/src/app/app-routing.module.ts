2ac68a983fc60ee828400acac2957368e4d1af14
humbertpa <humbertpal@gmail.com>
2023-03-29 01:52:53 -0600

    tarea terminada

--- Tarea6/src/app/app-routing.module.ts
+++ Tarea6/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { EducacionComponent } from './pages/educacion/educacion.component';
+import { IndexComponent } from './pages/index/index.component';
+import { NoPaginaComponent } from './pages/no-pagina/no-pagina.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'educacion', component: EducacionComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: '**', component: NoPaginaComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Tarea6/src/app/app.module.ts
+++ Tarea6/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { SidenavComponent } from './layouts/sidenav/sidenav.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { IndexComponent } from './pages/index/index.component';
+import { NoPaginaComponent } from './pages/no-pagina/no-pagina.component';
+import { EducacionComponent } from './pages/educacion/educacion.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    ContactoComponent,
+    IndexComponent,
+    NoPaginaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
